cfb23869adf2eada0c564c28094cf2b58f391598
basslines <admir.osmanovic@gmail.com>
2021-03-25 00:49:12 +0100

    dodao index.js fajl

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+var users = [
+  { firstName: "John", lastName: "Doe", age: 24, gender: "male" },
+  { firstName: "Jane", lastName: "Doe", age: 5, gender: "female" },
+  { firstName: "Jim", lastName: "Carrey", age: 54, gender: "male" },
+  { firstName: "Kate", lastName: "Winslet", age: 40, gender: "female" },
+];
+
+function getUsers() {
+ 
+  }
+
+function findUser(lastName, gender) {
+}
